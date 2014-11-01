e9ac742331890326931deadcb640db12e4af0c4e
Santhosh Kumar Manavasi Lakshminarayanan <santhoshkumarml@gmail.com>
2014-11-01 01:06:31 -0400

    Adding log Message

---
+++ Async-Models/src/main/java/async/chainreplication/communication/messages/LogMessage.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package async.chainreplication.communication.messages;
+
+/**
+ * @author Santhosh Kumar Manavasi Lakshminarayanan
+ *
+ */
+public class LogMessage extends ChainReplicationMessage {
+
+	private static final long serialVersionUID = -8797873379311671762L;
+
+	String message;
+	
+	public LogMessage(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	@Override
+	public String toString() {
+		return "LogMessage [message=" + message + "]";
+	}
+
+}
