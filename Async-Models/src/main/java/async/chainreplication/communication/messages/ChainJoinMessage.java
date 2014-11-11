6b3e461d3f2cd9124bbe2628d3e47cb5105d50ef
Santhosh Kumar Manavasi Lakshminarayanan <santhoshkumarml@gmail.com>
2014-11-10 22:26:35 -0500

    Refactoring

--- Async-Models/src/main/java/async/chainreplication/communication/messages/ChainJoinMessage.java
+++ Async-Models/src/main/java/async/chainreplication/communication/messages/ChainJoinMessage.java
@@ -1,14 +1,23 @@
 package async.chainreplication.communication.messages;
 
-public class ChainJoinMessage extends ChainReplicationMessage {
+import async.chainreplication.master.models.Server;
 
-	/**
-	 * 
-	 */
+/**
+ * The Class ChainJoinMessage.
+ */
+public class ChainJoinMessage extends ChainReplicationMessage {
+	
+	Server server;
+
+	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = 2971820276311529915L;
 
-	public ChainJoinMessage() {
+	/**
+	 * Instantiates a new chain join message.
+	 */
+	public ChainJoinMessage(Server server) {
 		super(Priority.REALTIME_PRIORITY);
+		this.server = server;
 	}
 
 }
