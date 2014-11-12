6cb96a9b7008c2ccd85d0ba1061e806738961aff
Santhosh Kumar Manavasi Lakshminarayanan <santhoshkumarml@gmail.com>
2014-11-11 21:39:47 -0500

    Completing ChainExtension

---
+++ Async-Models/src/main/java/async/chainreplication/communication/messages/MasterChainJoinReplyMessage.java
@@ -0,0 +1,47 @@
+package async.chainreplication.communication.messages;
+
+import async.chainreplication.master.models.Server;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class MasterServerChangeMessage.
+ */
+public class MasterChainJoinReplyMessage extends MasterMessage {
+
+
+	/** The Constant serialVersionUID. */
+	private static final long serialVersionUID = 7574520044298891698L;
+
+	/** The server. */
+	Server exisistingTail;
+
+	/**
+	 * Instantiates a new master server change message.
+	 *
+	 * @param exisistingTail the exisisting tail
+	 */
+	public MasterChainJoinReplyMessage(Server exisistingTail) {
+		super(Priority.REALTIME_PRIORITY);
+		this.exisistingTail = exisistingTail;
+	}
+
+	/**
+	 * Gets the server.
+	 *
+	 * @return the server
+	 */
+	public Server getExisistingTail() {
+		return exisistingTail;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "MasterChainJoinReplyMessage [exisistingTail=" + exisistingTail
+				+ "]";
+	}
+
+}
